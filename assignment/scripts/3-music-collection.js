46758473e795fd521a33a2f6c5cf265dd17475e6
Vince Saykao <vincesaykao@Vinces-MacBook-Air.local>
2022-01-12 19:18:30 -0600

    added object literal and function addToCollection

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -1,1 +1,13 @@
 console.log('***** Music Collection *****')
+
+collection = [];
+
+function addToCollection(title,artist,yearPublished) {
+
+}
+
+album = {
+  album.title: 'Good Things Fall Apart',
+  album.artist: 'Illenium',
+  artist.yearPublished: 2020
+}
