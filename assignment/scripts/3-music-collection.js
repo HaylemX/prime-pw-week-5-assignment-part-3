232686bfc7646b8c3c196b83fbcfc80da55ceefb
Vince Saykao <vincesaykao@Vinces-MacBook-Air.local>
2022-01-12 22:09:31 -0600

    completed addToCollection function

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -7,9 +7,10 @@ let collection = []
 function addToCollection(title, artist, yearPublished) {
   let obj = {}
 
-    obj.title = title;
-    obj.artist = artist;
-    obj.yearPublished = yearPublished;
+    obj.title = title,
+    obj.artist = artist,
+    obj.yearPublished = yearPublished
+
 
 collection.push(obj.title,obj.artist,obj.yearPublished);
 
@@ -17,10 +18,26 @@ collection.push(obj.title,obj.artist,obj.yearPublished);
   // return object
   }
 
+  const myList = new addToCollection('Good Things Fall Apart','Illenium', 2020);
+  const myList1 = new addToCollection('Truthhurts','Blah', 2021);
+  const myList2 = new addToCollection('Call of Faith','Shining', 1923);
+  const myList3 = new addToCollection('Truthhurts','Blah', 2021);
+  const myList4 = new addToCollection('Happy Life','Movies', 2025);
+  const myList5 = new addToCollection('Blues','Neat', 2024);
+  // adding addToCollection parameter values
 
+  collection.push(myList, myList1,myList2,myList3,myList4,myList5);
+  // pushing new objects into collection array
 
+  console.log(myList);
+  console.log(myList1);
+  console.log(myList2);
+  console.log(myList3);
+  console.log(myList4);
+  console.log(myList5);
+  // console logs all albums
 
-console.log(addToCollection( collection ));
+console.log(collection);
 
 
 
